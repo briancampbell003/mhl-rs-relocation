d241ac8edfec9df9be98398c0f2455ff95ced902
Brian Campbell <brian.campbell003@gmail.com>
2023-02-20 18:18:04 -0600

    styling-home page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import Navigation from './Navigation';
+import mainLogo from '../../assets/main-logo.jpg';
+
 
 function Header({ currentPage, handlePageChange }) {
     return (
+      <div>
+        <img class="mhl-corner-logo" src={mainLogo} alt="MHL logo" width="375" height="90"></img>
+        
         <div className="header">
-          {/* <h1 className="header-mhl-title">RESIDENTS SERVICES RESYNDICATION GUIDE</h1> */}
             <a
-            href="#home"
-            onClick={() => handlePageChange('Home')}
-          >
-            RESIDENTS SERVICES RESYNDICATION GUIDE
-          </a>
-          <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
-        </div>
+              href="#home"
+              onClick={() => handlePageChange('Home')}
+            >
+              RESIDENTS SERVICES RESYNDICATION GUIDE
+            </a>
+            <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
+          </div>
+      </div>
       );
 }
 
