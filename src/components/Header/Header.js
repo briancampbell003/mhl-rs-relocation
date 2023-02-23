965382a02c77216214f567d057229641f1d65446
Brian Campbell <brian.campbell003@gmail.com>
2023-02-23 15:22:46 -0600

    mobile responsive tweaks

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navigation from './Navigation';
 import mainLogo from '../../assets/main-logo-trans.png';
+// import smallLogo from '../../assets/main-logo-trans.png';
 
 
 function Header({ currentPage, handlePageChange }) {
     return (
       <div>
         <img className="mhl-corner-logo" src={mainLogo} alt="MHL logo" width="250" height="60"></img>
+        {/* <img className="mhl-small-logo" src={smallLogo} alt="MHL logo" width="250" height="60"></img> */}
         
         <div className="header">
             <a
